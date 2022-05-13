ea0ff1de51c29dd5e5094cbfb5fd4cf3739620df
Savinda Senevirathne <savindadilsara@gmail.com>
2022-05-13 11:02:03 +0530

    Initial

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,20 +1,21 @@
 import React, { Fragment } from 'react';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import {
-  Avatar,
-  IconButton,
-  Box,
   Card,
   CardContent
 } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
-import avatar1 from '../../assets/images/avatars/avatar1.jpg';
-import avatar2 from '../../assets/images/avatars/avatar2.jpg';
-import avatar3 from '../../assets/images/avatars/avatar3.jpg';
+export default function Table(props) {
 
-export default function Table() {
+  const history = useHistory();
+
+  const handleRowClick = (e, address) => {
+    console.log(address);
+    history.push(`/hosts/${address}`);
+    e.preventDefault();
+  }
+  console.log(props.hosts);
   return (
     <Fragment>
       <Card className="card-box mb-4">
@@ -23,105 +24,82 @@ export default function Table() {
             <table className="table table-striped table-hover text-nowrap mb-0">
               <thead className="thead-light">
                 <tr>
-                  <th style={{ width: '40%' }}>Employee</th>
-                  <th className="text-center">Status</th>
-                  <th className="text-center">Actions</th>
+                  <th style={{ width: '30%' }}>Address</th>
+                  <th className="text-center">Country Code</th>
+                  <th className="text-center">CPU</th>
+                  <th className="text-center">Ram</th>
+                  <th className="text-center">Disk</th>
+                  <th className="text-center">Total Instances</th>
+                  <th className="text-center">Active Instances</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>
-                    <div className="d-flex align-items-center">
-                      <Avatar alt="..." src={avatar2} className="mr-2" />
-                      <div>
-                        <a
-                          href="#/"
-                          onClick={e => e.preventDefault()}
-                          className="font-weight-bold text-black"
-                          title="...">
-                          Shanelle Wynn
-                        </a>
-                        <span className="text-black-50 d-block">
-                          UI Engineer, Apple Inc.
-                        </span>
+                {props.hosts.map((host, index) => (
+                  <tr key={index} onClick={e => handleRowClick(e, host.fields.address.stringValue)} style={{cursor: 'pointer'}}>
+                    <td>
+                      <div className="d-flex align-items-center">
+                        <div>
+                          <a
+                            href="#/"
+                            className="font-weight-bold text-black"
+                            title="...">
+                            {host.fields.address.stringValue}
+                          </a>
+                          <span className="text-black-50 d-block py-1">
+                            {
+                              host.fields.version &&
+                              <span>Version: {host.fields.version?.stringValue} | </span>
+                            }
+                            {
+                              host.fields.description &&
+                              <span>Description: {host.fields.description?.stringValue}</span>
+                            }
+                          </span>
+                        </div>
                       </div>
-                    </div>
-                  </td>
-                  <td className="text-center">
-                    <div className="h-auto py-0 px-3 badge badge-warning">
-                      Pending
-                    </div>
-                  </td>
-                  <td className="text-center">
-                    <Box>
-                      <IconButton color="primary" size="small">
-                        <FontAwesomeIcon icon={['fas', 'ellipsis-h']} />
-                      </IconButton>
-                    </Box>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <div className="d-flex align-items-center">
-                      <Avatar alt="..." src={avatar1} className="mr-2" />
-                      <div>
-                        <a
-                          href="#/"
-                          onClick={e => e.preventDefault()}
-                          className="font-weight-bold text-black"
-                          title="...">
-                          Beck Simpson
-                        </a>
-                        <span className="text-black-50 d-block">
-                          Frontend Developer
-                        </span>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="text-center">
-                    <div className="badge badge-success h-auto py-0 px-3">
-                      Completed
-                    </div>
-                  </td>
-                  <td className="text-center">
-                    <Box>
-                      <IconButton color="primary" size="small">
-                        <FontAwesomeIcon icon={['fas', 'ellipsis-h']} />
-                      </IconButton>
-                    </Box>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <div className="d-flex align-items-center">
-                      <Avatar alt="..." src={avatar3} className="mr-2" />
-                      <div>
-                        <a
-                          href="#/"
-                          onClick={e => e.preventDefault()}
-                          className="font-weight-bold text-black"
-                          title="...">
-                          Regan Norris
-                        </a>
-                        <span className="text-black-50 d-block">
-                          Senior Project Manager
-                        </span>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="text-center">
-                    <div className="h-auto py-0 px-3 badge badge-danger">
-                      Declined
-                    </div>
-                  </td>
-                  <td className="text-center">
-                    <Box>
-                      <IconButton color="primary" size="small">
-                        <FontAwesomeIcon icon={['fas', 'ellipsis-h']} />
-                      </IconButton>
-                    </Box>
-                  </td>
-                </tr>
+                    </td>
+                    <td className="text-center">
+                      {/* <div className="h-auto py-0 px-3 badge badge-warning">
+                        Pending
+                      </div> */}
+                      {host.fields.country_code.stringValue}
+                    </td>
+                    <td className="text-center">
+                      {/* <div className="h-auto py-0 px-3 badge badge-warning">
+                        Pending
+                      </div> */}
+                      {host.fields.cpu_microsec.integerValue}
+                    </td>
+                    <td className="text-center">
+                      {/* <div className="h-auto py-0 px-3 badge badge-warning">
+                        Pending
+                      </div> */}
+                      {host.fields.ram_mb.integerValue}
+                    </td>
+                    <td className="text-center">
+                      {/* <div className="h-auto py-0 px-3 badge badge-warning">
+                        Pending
+                      </div> */}
+                      {host.fields.disk_mb.integerValue}
+                    </td>
+                    <td className="text-center">
+                      {/* <Box>
+                        <IconButton color="primary" size="small">
+                          <FontAwesomeIcon icon={['fas', 'ellipsis-h']} />
+                        </IconButton>
+                      </Box> */}
+                      {host.fields.no_of_total_instances.integerValue}
+                    </td>
+                    <td className="text-center">
+                      {/* <Box>
+                        <IconButton color="primary" size="small">
+                          <FontAwesomeIcon icon={['fas', 'ellipsis-h']} />
+                        </IconButton>
+                      </Box> */}
+                      {host.fields.no_of_active_instances.integerValue}
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
--- src/pages/Hosts/index.js
+++ src/pages/Hosts/index.js
@@ -1,14 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import PageTitle from '../../layout-components/PageTitle';
 import Table from '../../components/Table';
 
 export default function Hosts() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [hosts, setHosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('https://firestore.googleapis.com/v1/projects/evernodeindex/databases/(default)/documents/rHQQq5aJ5kxFyNJXE36rAmuhxpDvpLHcWq_hosts');
+      const data = await res.json();
+      setHosts(data.documents);
+      setIsLoaded(true);
+    }
+    fetchData();
+  }, []);
   return (
     <Fragment>
       <PageTitle
         titleHeading="Hosts"
       />
-      <Table />
+      <Table hosts={hosts} />
     </Fragment>
   );
 }
