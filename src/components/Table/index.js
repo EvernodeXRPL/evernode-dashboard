f8367257283b0a1e31e0c662424f0fbc2af793a0
chalith <desaman.chalith@gmail.com>
2022-05-17 17:12:00 +0530

    Changed host info index keys and table refactor

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -6,79 +6,35 @@ import {
 } from '@material-ui/core';
 
 export default function Table(props) {
+  const {
+    headings,
+    values,
+    hideHeadings,
+    onRowClick
+  } = props;
 
-  const { hosts, onRowClick } = props;
+  const keys = Object.keys(headings);
   return (
     <Fragment>
       <Card className="card-box mb-4">
         <CardContent className="p-0">
           <div className="table-responsive">
             <table className="table table-striped table-hover text-nowrap mb-0">
-              <thead className="thead-light">
+              {!hideHeadings && <thead className="thead-light">
                 <tr>
-                  <th style={{ width: '30%' }}>Address</th>
-                  <th className="text-center">Status</th>
-                  <th className="text-center">Country Code</th>
-                  <th className="text-center">CPU</th>
-                  <th className="text-center">Ram</th>
-                  <th className="text-center">Disk</th>
-                  <th className="text-center">Total Instances</th>
-                  <th className="text-center">Active Instances</th>
+                  {keys.map((k, i) => {
+                    return <th key={i}>{headings[k]}</th>
+                  })}
                 </tr>
-              </thead>
+              </thead>}
               <tbody>
-                {hosts.map((host, index) => (
-                  <tr key={index} onClick={() => onRowClick(host)} style={{ cursor: 'pointer' }}>
-                    <td>
-                      <div className="d-flex align-items-center">
-                        <div>
-                          <a
-                            href="#/"
-                            className="font-weight-bold text-black"
-                            title="...">
-                            {host.address}
-                          </a>
-                          <span className="text-black-50 d-block py-1">
-                            {
-                              host.version &&
-                              <span>Version: {host.version} | </span>
-                            }
-                            {
-                              host.description &&
-                              <span>Description: {host.description}</span>
-                            }
-                          </span>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="text-center">
-                      { (host.active && 
-                        <div className="h-auto py-2 px-3 badge badge-success">
-                        Active
-                      </div>
-                      ) || (<div className="h-auto py-2 px-3 badge badge-warning">
-                      Inactive
-                    </div>)}
-                      
-                    </td>
-                    <td className="text-center">
-                      {host.countryCode}
-                    </td>
-                    <td className="text-center">
-                      {host.cpuMicrosec}
-                    </td>
-                    <td className="text-center">
-                      {host.ramMb}
-                    </td>
-                    <td className="text-center">
-                      {host.diskMb}
-                    </td>
-                    <td className="text-center">
-                      {host.noOfTotalInstances}
-                    </td>
-                    <td className="text-center">
-                      {host.noOfActiveInstances}
-                    </td>
+                {values.map((value, i) => (
+                  <tr key={i} onClick={() => onRowClick(value)} style={{ cursor: 'pointer' }}>
+                    {keys.map((k, j) => {
+                      return <td key={j}>
+                        {value[k]}
+                      </td>
+                    })}
                   </tr>
                 ))}
               </tbody>
--- src/pages/Hosts/index.js
+++ src/pages/Hosts/index.js
@@ -3,7 +3,7 @@ import { useHistory } from "react-router-dom";
 
 import PageTitle from '../../layout-components/PageTitle';
 import Table from '../../components/Table';
-import { useEvernode } from '../../services/evernode';
+import { useEvernode } from '../../services/Evernode';
 import Loader from '../../components/Loader';
 
 export default function Hosts() {
@@ -16,14 +16,67 @@ export default function Hosts() {
   useEffect(() => {
     async function getData() {
       const data = await evernode.getHosts();
-      setIsLoaded(true)
-      setHosts(data);
+      setIsLoaded(true);
+      const tableHeadings = {
+        address: "Address",
+        status: "Status",
+        countryCode: "Country Code",
+        cpu: "CPU",
+        ram: "RAM",
+        disk: "Disk",
+        maxInstances: "Max Instances",
+        activeInstances: "Active Instances"
+      };
+      const tableValues = data.map(host => {
+        return {
+          key: host.address,
+          address: <div className="d-flex align-items-center">
+            <div>
+              <a
+                href="#/"
+                className="font-weight-bold text-black"
+                title="...">
+                {host.address}
+              </a>
+              <span className="text-black-50 d-block py-1">
+                {
+                  host.version &&
+                  <span>Version: {host.version} | </span>
+                }
+                {
+                  host.description &&
+                  <span>Description: {host.description}</span>
+                }
+              </span>
+            </div>
+          </div>,
+          status: host.active ?
+            <div className="h-auto py-2 px-3 badge badge-success">
+              Active
+            </div> :
+            <div className="h-auto py-2 px-3 badge badge-warning">
+              Inactive
+            </div>,
+          countryCode: host.countryCode,
+          cpu: host.cpuMicrosec,
+          ram: host.ramMb,
+          disk: host.diskMb,
+          maxInstances: host.maxInstances,
+          activeInstances: host.activeInstances
+        }
+      });
+
+      setHosts({
+        hosts: data,
+        tableHeadings: tableHeadings,
+        tableValues: tableValues
+      });
     };
     getData();
   }, [evernode]);
 
   const handleRowClick = useCallback((e) => {
-    history.push(`/host/${e.address}`);
+    history.push(`/host/${e.key}`);
   }, [history]);
 
   return (
@@ -31,7 +84,7 @@ export default function Hosts() {
       <PageTitle
         titleHeading="Hosts"
       />
-      {(isLoaded && <Table hosts={hosts} onRowClick={handleRowClick} />) ||
+      {(isLoaded && <Table headings={hosts.tableHeadings} values={hosts.tableValues} onRowClick={handleRowClick} />) ||
         <Loader className="p-4" />}
     </Fragment>
   );
