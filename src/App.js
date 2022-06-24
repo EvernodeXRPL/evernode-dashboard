6f777276108e74f672a229afd5ea900791105a0c
Kithmini Gunawardhana <kithminishanika@gmail.com>
2022-06-24 17:49:14 +0530

    Modification with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Provider } from 'react-redux';
 import configureStore from './config/configureStore';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
 import ScrollToTop from './utils/ScrollToTop';
 import Routes from './Routes';
@@ -12,12 +12,12 @@ const store = configureStore();
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter basename="/">
+      <HashRouter basename="/">
         <CssBaseline />
         <ScrollToTop>
           <Routes />
         </ScrollToTop>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   );
 }
