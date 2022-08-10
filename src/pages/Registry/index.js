9dbb632db140db73d321f4ab69aba6a8207e0b2a
Udith Indrakantha <45740208+Udith-Gayan@users.noreply.github.com>
2022-08-10 12:01:22 +0530

    Displaying EVR Rewarding Details (#22)

--- src/pages/Registry/index.js
+++ src/pages/Registry/index.js
@@ -8,7 +8,8 @@ import {
   Card,
   CardContent,
   Typography,
-  Tooltip
+  Tooltip,
+  CardHeader
 } from '@material-ui/core';
 
 import { useEvernode } from '../../services/Evernode';
@@ -17,8 +18,10 @@ import Loader from '../../components/Loader';
 export default function Registry() {
   const evernode = useEvernode();
 
-  const [configs, setConfigs] = React.useState(null);
+  const [registryConfigs, setRegistryConfigs] = React.useState(null);
   const [registryAddress, setRegistryAddress] = React.useState(null);
+  const [rewardConfigs, setRewardConfigs] = React.useState(null);
+
 
   useEffect(() => {
     const fetchConfigs = async () => {
@@ -28,10 +31,13 @@ export default function Registry() {
         key: 'Key',
         value: 'Value'
       }
-      const tableValues = [
+      const registryConfigTableValues = [
         {
           key: 'EVR Issuer XRP Address',
-          value: <Tooltip title="EVR Issuer XRP account address"><span>{config.evrIssuerAddress}</span></Tooltip>
+          value: <Tooltip title="EVR Issuer XRP account address"><span>{config.evrIssuerAddress}</span></Tooltip>,
+          cellConfigs: {
+            width: '37%'
+          }
         },
         {
           key: 'Foundation XRP Address',
@@ -57,15 +63,73 @@ export default function Registry() {
           key: 'Moment Size',
           value: <Tooltip title="No. of XRP ledgers per moment"><span>{config.momentSize}</span></Tooltip>
         },
+        {
+          key: 'Host Count',
+          value: <Tooltip title="Total number of registered hosts"><span>{config.hostCount}</span></Tooltip>
+        },
         // {
         //   key: 'Purchaser Target Price',
         //   value: <Tooltip title="Per moment lease amount that is derived from the condition of the epoch"><span>{config.purchaserTargetPrice}</span></Tooltip>
         // }
       ];
-      setConfigs({
+      setRegistryConfigs({
         configs: config,
         tableHeadings: tableHeadings,
-        tableValues: tableValues
+        tableValues: registryConfigTableValues
+      });
+
+      const rewardConfigTableValues = [
+        {
+          key: 'Epoch Count',
+          value: <Tooltip title="Total no. of epochs"><span>{config.rewardConfiguaration.epochCount}</span></Tooltip>,
+          cellConfigs: {
+            width: '37%'
+          }
+        },
+        {
+          key: 'Epoch Reward Amount',
+          value: <Tooltip title="Total amount of EVRs rewarded in one epoch"><span>{config.rewardConfiguaration.epochRewardAmount}</span></Tooltip>
+        },
+        {
+          key: 'First Epoch Reward Quota',
+          value: <Tooltip title="EVRs rewarded per moment within the first epoch"><span>{config.rewardConfiguaration.firstEpochRewardQuota}</span></Tooltip>
+        },
+        {
+          key: 'Reward Start Moment',
+          value: <Tooltip title="The moment EVR rewarding starts"><span>{config.rewardConfiguaration.rewardStartMoment}</span></Tooltip>
+        },
+        {
+          key: <Typography style={{ fontSize: '1.54rem', fontWeight: 'bold', color: 'black' }}>Reward Info</Typography>,
+          value: <Tooltip title=""><span>&nbsp;</span></Tooltip>,
+          cellConfigs: {
+            colspan: 2,
+            isSubtopic: true,
+            width: '37%',
+            paddingTopBottom: '8px'
+
+          }
+        },
+        {
+          key: 'Current Epoch',
+          value: <Tooltip title="Current epoch"><span>{config.rewardInfo.epoch}</span></Tooltip>
+        },
+        {
+          key: 'Epoch Pool',
+          value: <Tooltip title="Available amount of EVRs in the current epoch's reward pool"><span>{+(+config.rewardInfo.epochPool).toFixed(3)}</span></Tooltip>
+        },
+        {
+          key: `Active Host Count of the Moment ${config.rewardInfo.savedMoment}`,
+          value: <Tooltip title={`No. of Active hosts in the moment ${config.rewardInfo.savedMoment}`}><span>{config.rewardInfo.curMomentActiveHostCount}</span></Tooltip>
+        },
+        {
+          key: `Active Host Count of the Moment ${config.rewardInfo.savedMoment - 1}`,
+          value: <Tooltip title={`No. of Active hosts in the moment ${config.rewardInfo.savedMoment - 1}`}><span>{config.rewardInfo.prevMomentActiveHostCount}</span></Tooltip>
+        }
+      ];
+      setRewardConfigs({
+        configs: config,
+        tableHeadings: tableHeadings,
+        tableValues: rewardConfigTableValues
       });
     }
 
@@ -82,9 +146,9 @@ export default function Registry() {
         <Grid item xs={12}>
           <Card style={{ border: "none", boxShadow: "none" }} className="mb-4 bg-transparent">
             <CardContent className="p-0">
-              {(configs && <RegularTable
-                headings={configs.tableHeadings}
-                values={configs.tableValues}
+              {(registryConfigs && <RegularTable
+                headings={registryConfigs.tableHeadings}
+                values={registryConfigs.tableValues}
                 highlight={['key']}
                 hideHeadings />) ||
                 <Loader className="p-4" />}
@@ -92,6 +156,21 @@ export default function Registry() {
           </Card>
         </Grid>
       </Grid>
-    </Fragment >
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <Card style={{ border: "none", boxShadow: "none" }} className="mb-4">
+            <CardHeader className="pt-3 pb-2" title={<Typography style={{ fontSize: '1.54rem', fontWeight: 'bold' }} >Reward Configurations</Typography>} />
+            <CardContent className="p-0">
+              {(rewardConfigs && <RegularTable
+                headings={rewardConfigs.tableHeadings}
+                values={rewardConfigs.tableValues}
+                highlight={['key']}
+                hideHeadings />) ||
+                <Loader className="p-4" />}
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Fragment>
   );
 }
