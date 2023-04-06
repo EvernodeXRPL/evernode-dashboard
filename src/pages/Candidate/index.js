eb18ab98fd61cf165e98c2970136843a8387f27c
Kasun Kavinda <kasunkavinda.bc@gmail.com>
2023-04-06 15:15:42 +0530

    candidate view changes

--- src/pages/Candidate/index.js
+++ src/pages/Candidate/index.js
@@ -1,8 +1,5 @@
 import React, { Fragment, useCallback, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
-
-import EditIcon from '@material-ui/icons/Edit';
-
 import PageTitle from '../../layout-components/PageTitle';
 import RegularTable from '../../components/RegularTable';
 
@@ -10,92 +7,28 @@ import {
     Grid,
     Card,
     CardContent,
-    Typography,
     Tooltip,
-    TextField,
-    Button,
-    Hidden
 } from '@material-ui/core';
-import { makeStyles } from "@material-ui/core/styles";
-import Leases from '../../business-components/Leases';
 
 import { useEvernode } from '../../services/Evernode';
 import Loader from '../../components/Loader';
 import { StorageKeys } from '../../common/constants';
-import CountryFlag from '../../business-components/CountryFlag';
-import EvrBalance from '../../business-components/EvrBalance';
-import CPUModel from '../../business-components/CPUModel';
-import InstanceSpecs from '../../business-components/InstanceSpecs';
-import ModalDialog from '../../components/ModalDialog';
-
-const useStyles = makeStyles({
-    root: {
-        // input label when focused
-        "& label.Mui-focused": {
-            color: 'rgba(0,0,0,0.54)'
-        },
-        "& label.Mui-error": {
-            color: 'red'
-        },
-        // focused color for input with variant='standard'
-        "& .MuiInput-underline:after": {
-            borderBottomColor: 'rgba(0,0,0,0.87)'
-        },
-        "& .MuiInput-underline.Mui-error:after": {
-            borderBottomColor: '#f83245'
-        },
-        "& label.MuiInputLabel-shrink": {
-            transform: 'translate(0, 1.5px) scale(0.95)',
-            transformOrigin: 'top left'
-        }
-    }
-});
 
 const Candidate = (props) => {
-    const classes = useStyles();
     const history = useHistory();
     const evernode = useEvernode();
 
     const selfAddress = localStorage.getItem(StorageKeys.hostAddress);
-    console.log("props.match.params.address", props.match.params.candidateId)
     const pathAddress = props.match.params.candidateId;
 
     const [address, setAddress] = React.useState(pathAddress || selfAddress);
-    const [inputAddress, setInputAddress] = React.useState(null);
     const [info, setInfo] = React.useState(null);
-    const [showChangeAddress, setShowChangeAddress] = React.useState(false);
-
-    const inputAddressValid = useCallback(() => {
-        return /^r[a-zA-Z0-9]{24,34}$/g.test(inputAddress);
-    }, [inputAddress])
-
-    const handleChangeAddress = useCallback(() => {
-        if (inputAddress && inputAddressValid()) {
-            localStorage.setItem(StorageKeys.hostAddress, inputAddress);
-            setAddress(inputAddress);
-            setInputAddress(null);
-            setShowChangeAddress(false);
-        }
-    }, [inputAddress, inputAddressValid]);
-
-    const handleChangeAddressClose = useCallback(() => {
-        setShowChangeAddress(false);
-        setInputAddress(null);
-        // If the address change modal is closed without changing the address,
-        // Redirect to the home page.
-        if (!address)
-            history.push('/')
-    }, [address, history]);
 
     useEffect(() => {
         const fetchInfo = async () => {
             setInfo(null);
-            console.log('candidateIdddd', address)
             const hosts = await evernode.getCandidateById(address);
-            console.log('hosts', hosts)
-            const config = await evernode.getConfigs();
-            const hostInfo = hosts;
-            console.log('hostInfo', hostInfo)
+            const hostInfo = (hosts) ? hosts : null;
             const tableHeadings = {
                 key: 'Key',
                 value: 'Value'
@@ -149,25 +82,15 @@ const Candidate = (props) => {
                     </Tooltip>
                 },
             ] : [];
-            if (hostInfo?.registrationTimestamp)
-                tableValues.push(
-                    {
-                        key: 'Registered on Timestamp',
-                        value: <Tooltip title="Timestamp at which the host registered"><span>{hostInfo.registrationTimestamp}</span></Tooltip>
-                    });
-            // const evrBalance = await evernode.getEVRBalance(address);
             setInfo({
-                // evrBalance: evrBalance,
                 hostInfo: hostInfo,
                 tableHeadings: tableHeadings,
                 tableValues: tableValues
             });
         }
 
-        // If the path address param is empty this is My Host page and no address is set in local storage.
-        if (!address)
-            setShowChangeAddress(true);
-        else if (pathAddress === selfAddress)
+        // If no address is set in local storage.
+        if (pathAddress === selfAddress)
             history.push('/host');
         else
             fetchInfo();
@@ -184,27 +107,6 @@ const Candidate = (props) => {
                     }
                 >
                 </PageTitle>
-                <Grid container spacing={4}>
-                    {info && info.hostInfo && info.hostInfo.hostMessage ? (
-                        <Grid item xs={12}>
-                            <Card
-                                style={{ border: "none", boxShadow: "none" }}
-                                className="mb-4 bg-transparent"
-                            >
-                                <CardContent className="p-0">
-                                    <div className="p-3 border rounded host-message mb-0">
-                                        {(info &&
-                                            (info.hostInfo.hostMessage
-                                                ? info.hostInfo.hostMessage
-                                                : "There is no host message available!")) || (
-                                                <Loader className="p-4" />
-                                            )}
-                                    </div>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    ) : null}
-                </Grid>
                 <Grid container spacing={4}>
                     <Grid item xs={12} md={6}>
                         <Card
