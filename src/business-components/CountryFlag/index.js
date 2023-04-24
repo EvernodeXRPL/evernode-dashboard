3772932c988a4d017b7da635ea03c223fa7ec40a
e17100 <73388013+SachinthaMadhushanka@users.noreply.github.com>
2023-04-24 15:10:11 +0530

    Handle invalid country codes when showing country flag (#58)

--- src/business-components/CountryFlag/index.js
+++ src/business-components/CountryFlag/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Tooltip, withStyles } from '@material-ui/core';
 import ReactCountryFlag from 'react-country-flag';
+import ISO3166_1 from "iso-3166-1";
 
 export default function CountryFlag(props) {
   const { countryCode, size } = props;
+
+  const isValidCountryCode = (code) => {
+    return ISO3166_1.whereAlpha2(code) !== undefined;
+  };
 
   // Overriding tooltip styles to keep tooltip near the flag.
   const StyledTooltip = withStyles({
@@ -12,21 +17,27 @@ export default function CountryFlag(props) {
     },
   })(Tooltip);
 
-  return (
-    <StyledTooltip title={countryCode} placement='right-end'>
-      <div>
-        <ReactCountryFlag
-          className="emojiFlag"
-          countryCode={countryCode}
-          style={{
-            fontSize: size,
-            cursor: 'pointer'
-          }}
-          aria-label={countryCode}
-          alt={countryCode}
-          svg
-        />
-      </div>
-    </StyledTooltip>
-  );
+  if (isValidCountryCode(countryCode)) {
+    return (
+      <StyledTooltip title={countryCode} placement='right-end'>
+        <div>
+          <ReactCountryFlag
+            className="emojiFlag"
+            countryCode={countryCode}
+            style={{
+              fontSize: size,
+              cursor: 'pointer'
+            }}
+            aria-label={countryCode}
+            alt={countryCode}
+            svg
+          />
+        </div>
+      </StyledTooltip>
+    );
+  } else {
+    return (
+      <span style={{fontSize: "24px", width: "42px", textAlign: "center"}}>{countryCode}</span>
+    )
+  }
 }
